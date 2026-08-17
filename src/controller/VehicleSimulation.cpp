1f56a502bf005aad812e0efd9c86143bcb5a1601
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-17 21:58:34 +0530

    rfr-4.1

--- src/controller/VehicleSimulation.cpp
+++ src/controller/VehicleSimulation.cpp
@@ -2,10 +2,12 @@
 #include "model/VehicleData.h"
 
 #include <QDateTime>
+#include <algorithm>
 #include <cmath>
+#include <cstdint>
 
 VehicleSimulation::VehicleSimulation(QObject *parent)
-    : QObject(parent)
+    : QObject{parent}
 {
     connect(&m_timer, &QTimer::timeout, this, &VehicleSimulation::update);
 }
@@ -22,9 +24,9 @@ void VehicleSimulation::init()
         return;
     }
 
-    m_previousTime = QDateTime::currentMSecsSinceEpoch();
-    m_simTime = 0.0;
-    m_timer.start(16); // 60fps target
+    m_previousTime = static_cast<std::uint64_t>(QDateTime::currentMSecsSinceEpoch());
+    m_simTime = 0.0e0;
+    m_timer.start(static_cast<std::int32_t>(16));
 }
 
 void VehicleSimulation::stop()
@@ -37,12 +39,11 @@ void VehicleSimulation::update()
     if (!m_vehicle)
         return;
 
-    quint64 currentTime = QDateTime::currentMSecsSinceEpoch();
-    double dt = (currentTime - m_previousTime) / 1000.0;
+    std::uint64_t currentTime{static_cast<std::uint64_t>(QDateTime::currentMSecsSinceEpoch())};
+    double dt{(static_cast<double>(currentTime) - static_cast<double>(m_previousTime)) / 1.0e3};
 
-    
-    if (dt > 0.1)
-        dt = 0.016;
+    if (dt > 1.0e-1)
+        dt = 1.6e-2;
 
     m_previousTime = currentTime;
     m_simTime += dt;
@@ -52,77 +53,63 @@ void VehicleSimulation::update()
     simulateEngineTemp(dt);
 }
 
-void VehicleSimulation::simulateDriveDynamics(double /*dt*/)
+void VehicleSimulation::simulateDriveDynamics(double)
 {
-    
-    double t = std::fmod(m_simTime, 120.0);
-    double speed = 0.0;
-    double rpm = 800.0;
+    double t{std::fmod(m_simTime, 1.2e2)};
+    double speed{0.0e0};
+    double rpm{8.0e2};
 
-    if (t < 5.0) {
-        
-        speed = 0.0;
-        rpm = 800.0 + 30.0 * std::sin(m_simTime * 2.5);
-    } else if (t < 20.0) {
-        
-        double progress = (t - 5.0) / 15.0;
-        speed = 80.0 * progress * progress;
-        rpm = 800.0 + 3200.0 * progress;
-    } else if (t < 35.0) {
-        
-        speed = 80.0 + 2.0 * std::sin(m_simTime * 0.5);
-        rpm = 2600.0 + 80.0 * std::sin(m_simTime * 0.7);
-    } else if (t < 45.0) {
-        
-        double progress = (t - 35.0) / 10.0;
-        speed = 80.0 + 60.0 * progress;
-        rpm = 2600.0 + 2200.0 * progress;
-    } else if (t < 65.0) {
-        
-        speed = 140.0 + 3.0 * std::sin(m_simTime * 0.3);
-        rpm = 3800.0 + 100.0 * std::sin(m_simTime * 0.5);
-    } else if (t < 75.0) {
-        
-        double progress = (t - 65.0) / 10.0;
-        speed = 140.0 - 80.0 * progress;
-        rpm = 3800.0 - 1800.0 * progress;
-    } else if (t < 90.0) {
-        
-        speed = 55.0 + 12.0 * std::sin(m_simTime * 0.4);
-        rpm = 2000.0 + 400.0 * std::sin(m_simTime * 0.6);
-    } else if (t < 105.0) {
-        
-        double progress = (t - 90.0) / 15.0;
-        speed = 55.0 * (1.0 - progress * progress);
-        rpm = 2000.0 - 1200.0 * progress;
+    if (t < 5.0e0) {
+        speed = 0.0e0;
+        rpm = 8.0e2 + 3.0e1 * std::sin(m_simTime * 2.5e0);
+    } else if (t < 2.0e1) {
+        double progress{(t - 5.0e0) / 1.5e1};
+        speed = 8.0e1 * progress * progress;
+        rpm = 8.0e2 + 3.2e3 * progress;
+    } else if (t < 3.5e1) {
+        speed = 8.0e1 + 2.0e0 * std::sin(m_simTime * 5.0e-1);
+        rpm = 2.6e3 + 8.0e1 * std::sin(m_simTime * 7.0e-1);
+    } else if (t < 4.5e1) {
+        double progress{(t - 3.5e1) / 1.0e1};
+        speed = 8.0e1 + 6.0e1 * progress;
+        rpm = 2.6e3 + 2.2e3 * progress;
+    } else if (t < 6.5e1) {
+        speed = 1.4e2 + 3.0e0 * std::sin(m_simTime * 3.0e-1);
+        rpm = 3.8e3 + 1.0e2 * std::sin(m_simTime * 5.0e-1);
+    } else if (t < 7.5e1) {
+        double progress{(t - 6.5e1) / 1.0e1};
+        speed = 1.4e2 - 8.0e1 * progress;
+        rpm = 3.8e3 - 1.8e3 * progress;
+    } else if (t < 9.0e1) {
+        speed = 5.5e1 + 1.2e1 * std::sin(m_simTime * 4.0e-1);
+        rpm = 2.0e3 + 4.0e2 * std::sin(m_simTime * 6.0e-1);
+    } else if (t < 1.05e2) {
+        double progress{(t - 9.0e1) / 1.5e1};
+        speed = 5.5e1 * (1.0e0 - progress * progress);
+        rpm = 2.0e3 - 1.2e3 * progress;
     } else {
-        
-        speed = 0.0;
-        rpm = 800.0 + 30.0 * std::sin(m_simTime * 2.5);
+        speed = 0.0e0;
+        rpm = 8.0e2 + 3.0e1 * std::sin(m_simTime * 2.5e0);
     }
 
-    
-    speed += 0.2 * std::sin(m_simTime * 11.0);
-    rpm += 15.0 * std::sin(m_simTime * 9.0);
+    speed += 2.0e-1 * std::sin(m_simTime * 1.1e1);
+    rpm += 1.5e1 * std::sin(m_simTime * 9.0e0);
 
-    m_vehicle->setSpeed(std::max(0.0, speed));
-    m_vehicle->setRpm(std::max(750.0, rpm));
+    m_vehicle->setSpeed(std::max(0.0e0, speed));
+    m_vehicle->setRpm(std::max(7.5e2, rpm));
 }
 
 void VehicleSimulation::simulateFuel(double dt)
 {
-    
-    double rpmFraction = m_vehicle->rpm() / 8000.0;
-    double speedFraction = m_vehicle->speed() / 260.0;
+    double rpmFraction{m_vehicle->rpm() / 8.0e3};
+    double speedFraction{m_vehicle->speed() / 2.6e2};
 
-    
-    double consumptionPerSec = 0.000004 + rpmFraction * 0.00002 + speedFraction * 0.00003;
+    double consumptionPerSec{4.0e-6 + rpmFraction * 2.0e-5 + speedFraction * 3.0e-5};
 
-    double fuel = m_vehicle->fuelLevel() - consumptionPerSec * dt;
+    double fuel{m_vehicle->fuelLevel() - consumptionPerSec * dt};
 
-    
-    if (fuel < 0.05) {
-        fuel = 0.90;
+    if (fuel < 5.0e-2) {
+        fuel = 9.0e-1;
     }
 
     m_vehicle->setFuelLevel(fuel);
@@ -130,33 +117,26 @@ void VehicleSimulation::simulateFuel(double dt)
 
 void VehicleSimulation::simulateEngineTemp(double dt)
 {
-    
+    double rpmFraction{m_vehicle->rpm() / 8.0e3};
 
-    double rpmFraction = m_vehicle->rpm() / 8000.0;
+    double targetTemp{8.5e1 + rpmFraction * 2.0e1};
 
-    
-    double targetTemp = 85.0 + rpmFraction * 20.0;
-
-    
-    if (m_simTime < 30.0) {
-        double warmupProgress = m_simTime / 30.0;
-        targetTemp = 25.0 + (targetTemp - 25.0) * warmupProgress * warmupProgress;
+    if (m_simTime < 3.0e1) {
+        double warmupProgress{m_simTime / 3.0e1};
+        targetTemp = 2.5e1 + (targetTemp - 2.5e1) * warmupProgress * warmupProgress;
     }
 
-    double currentTemp = m_vehicle->engineTemp();
+    double currentTemp{m_vehicle->engineTemp()};
 
-    
-    double thermalResponseRate = 0.016; // ~1/60 per second
-    double tempDelta = (targetTemp - currentTemp) * thermalResponseRate * dt;
+    double thermalResponseRate{1.6e-2};
+    double tempDelta{(targetTemp - currentTemp) * thermalResponseRate * dt};
 
-    
-    double noise = 0.3 * std::sin(m_simTime * 1.7) + 0.2 * std::sin(m_simTime * 0.3);
+    double noise{3.0e-1 * std::sin(m_simTime * 1.7e0) + 2.0e-1 * std::sin(m_simTime * 3.0e-1)};
 
-    double newTemp = currentTemp + tempDelta + noise * dt;
+    double newTemp{currentTemp + tempDelta + noise * dt};
 
-    
-    if (newTemp < 20.0) newTemp = 20.0;
-    if (newTemp > 120.0) newTemp = 120.0;
+    if (newTemp < 2.0e1) newTemp = 2.0e1;
+    if (newTemp > 1.2e2) newTemp = 1.2e2;
 
     m_vehicle->setEngineTemp(newTemp);
 }
