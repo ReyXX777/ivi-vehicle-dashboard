f0423ec58f8c1553c865a957320946a35733ef81
AbhishekAzadXx77 <reyanshroyabhi77@gmail.com>
2026-08-20 23:22:38 +0530

    rfr-5.1

--- src/model/VehicleData.cpp
+++ src/model/VehicleData.cpp
@@ -1,8 +1,24 @@
 #include "VehicleData.h"
 #include <cmath>
 
-VehicleData::VehicleData(QObject *parent)
-    : QObject{parent}
+namespace model {
+
+namespace {
+    constexpr double g_minSpeed{0.0e0};
+    constexpr double g_maxSpeed{3.0e2};
+
+    constexpr double g_minRpm{0.0e0};
+    constexpr double g_maxRpm{8.0e3};
+
+    constexpr double g_minFuel{0.0e0};
+    constexpr double g_maxFuel{1.0e0};
+
+    constexpr double g_minEngineTemp{5.0e1};
+    constexpr double g_maxEngineTemp{1.8e2};
+}
+
+VehicleData::VehicleData(QObject *l_parent)
+    : QObject{l_parent}
     , m_speed{0.0e0}
 {
 }
@@ -27,64 +43,72 @@ double VehicleData::engineTemp() const
     return m_engineTemp;
 }
 
-void VehicleData::setSpeed(double newSpeed)
+void VehicleData::setSpeed(double l_newSpeed)
 {
-    if (std::isnan(newSpeed))
+    if (::std::isnan(l_newSpeed))
         return;
 
-    if (qFuzzyCompare(m_speed, newSpeed))
+    if (::qFuzzyCompare(m_speed, l_newSpeed))
         return;
 
-    if (newSpeed < 0.0e0)
-        newSpeed = 0.0e0;
-    else if (newSpeed > 3.0e2)
-        newSpeed = 3.0e2;
+    if (l_newSpeed < g_minSpeed)
+        l_newSpeed = g_minSpeed;
+    else if (l_newSpeed > g_maxSpeed)
+        l_newSpeed = g_maxSpeed;
 
-    m_speed = newSpeed;
+    m_speed = l_newSpeed;
     emit speedChanged();
 }
 
-void VehicleData::setRpm(double newRpm)
+void VehicleData::setRpm(double l_newRpm)
 {
-    if (std::isnan(newRpm))
+    if (::std::isnan(l_newRpm))
         return;
-    if (qFuzzyCompare(m_rpm, newRpm))
-        return;
-    if (newRpm < 0.0e0)
-        newRpm = 0.0e0;
-    else if (newRpm > 8.0e3)
-        newRpm = 8.0e3;
 
-    m_rpm = newRpm;
+    if (::qFuzzyCompare(m_rpm, l_newRpm))
+        return;
+
+    if (l_newRpm < g_minRpm)
+        l_newRpm = g_minRpm;
+    else if (l_newRpm > g_maxRpm)
+        l_newRpm = g_maxRpm;
+
+    m_rpm = l_newRpm;
     emit rpmChanged();
 }
 
-void VehicleData::setFuelLevel(double newFuelLevel)
+void VehicleData::setFuelLevel(double l_newFuelLevel)
 {
-    if (std::isnan(newFuelLevel))
+    if (::std::isnan(l_newFuelLevel))
         return;
-    if (qFuzzyCompare(m_fuelLevel, newFuelLevel))
-        return;
-    if (newFuelLevel < 0.0e0)
-        newFuelLevel = 0.0e0;
-    else if (newFuelLevel > 1.0e0)
-        newFuelLevel = 1.0e0;
 
-    m_fuelLevel = newFuelLevel;
+    if (::qFuzzyCompare(m_fuelLevel, l_newFuelLevel))
+        return;
+
+    if (l_newFuelLevel < g_minFuel)
+        l_newFuelLevel = g_minFuel;
+    else if (l_newFuelLevel > g_maxFuel)
+        l_newFuelLevel = g_maxFuel;
+
+    m_fuelLevel = l_newFuelLevel;
     emit fuelLevelChanged();
 }
 
-void VehicleData::setEngineTemp(double newEngineTemp)
+void VehicleData::setEngineTemp(double l_newEngineTemp)
 {
-    if (std::isnan(newEngineTemp))
+    if (::std::isnan(l_newEngineTemp))
         return;
-    if (qFuzzyCompare(m_engineTemp, newEngineTemp))
-        return;
-    if (newEngineTemp < 5.0e1)
-        newEngineTemp = 5.0e1;
-    else if (newEngineTemp > 1.8e2)
-        newEngineTemp = 1.8e2;
 
-    m_engineTemp = newEngineTemp;
+    if (::qFuzzyCompare(m_engineTemp, l_newEngineTemp))
+        return;
+
+    if (l_newEngineTemp < g_minEngineTemp)
+        l_newEngineTemp = g_minEngineTemp;
+    else if (l_newEngineTemp > g_maxEngineTemp)
+        l_newEngineTemp = g_maxEngineTemp;
+
+    m_engineTemp = l_newEngineTemp;
     emit engineTempChanged();
 }
+
+} // namespace model
